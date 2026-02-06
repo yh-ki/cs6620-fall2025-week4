969cca0df14db07e9ed729d37e1ff9be66aecf4c
yh-ki <84252233+yh-ki@users.noreply.github.com>
2026-02-06 03:36:10 -0800

    Add arithmetic functions: subtract, multiply, divide

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ def add(a, b):
     """Add two numbers"""
     return a + b
 
+
 def subtract(a, b):
     """Subtract two numbers"""
     return a - b
 
+
 def multiply(a, b):
     """Multiply two numbers"""
     return a * b
+
 
 def divide(a, b):
     """Divide two numbers"""
