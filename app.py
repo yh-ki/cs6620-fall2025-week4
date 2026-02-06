610194a7c62591fbc0d083dc996be6067bf3cd65
Yuming Huang <yumingkirigara@gmail.com>
2026-02-06 01:50:24 -0800

    Second change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sys
 
 def add(a, b):
     """Add two numbers"""
-    return a+b
+    return a + b
 
 def subtract(a, b):
     """Subtract two numbers"""
@@ -12,13 +12,13 @@ def subtract(a, b):
 
 def multiply(a, b):
     """Multiply two numbers"""
-    return a*b
+    return a * b
 
 def divide(a, b):
     """Divide two numbers"""
     if b==0:
         raise ValueError("Cannot divide by zero")
-    return a/b
+    return a / b
 
 
 def calculate(operation, num1, num2):
